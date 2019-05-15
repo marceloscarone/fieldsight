e7993f7b5032f555db283f647f068d781c0b7ec8
awemulya <awemulya@gmail.com>
2019-05-15 10:33:14 +0545

    enketo edit improved

--- onadata/apps/fsforms/management/commands/enketo.py
+++ onadata/apps/fsforms/management/commands/enketo.py
@@ -2,7 +2,9 @@ import time
 
 from django.utils.translation import ugettext_lazy
 from optparse import make_option
+from pyvirtualdisplay import Display
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -18,18 +20,23 @@ USER_PASSWORD = ""
 
 # BASE_URL = "http://localhost:8001/"
 BASE_URL = "https://app.fieldsight.org/"
+# BASE_URL = "https://fieldsight.naxa.com.np/"
 # PROJECT_FORM_ID = 863944
 # ID_STRING = "aKj8xav4pECzcTW2zHm7SX"
 # ID_SUBMISSION = "42176"
 
 
-driver = webdriver.Firefox()
-
-
 def run_enketo(project_form, instance):
+    display = Display(visible=0, size=(1024, 900))
+    display.start()
+    firefox_capabilities = DesiredCapabilities.FIREFOX
+    firefox_capabilities['marionette'] = True
+    # firefox_capabilities['binary'] = '/home/awemulya/Downloads/geckodriver-v0.24.0-linux32'
+    driver = webdriver.Firefox(capabilities=firefox_capabilities, executable_path='/home/awemulya/Downloads/geckodriver-v0.24.0-linux32/geckodriver')
     driver.get(BASE_URL)
     driver.find_element_by_name('username').send_keys(USER_NAME)
     driver.find_element_by_name('password').send_keys(USER_PASSWORD + Keys.RETURN)
+    print(project_form)
     project_fxf = FieldSightXF.objects.get(pk=project_form)
     instances = project_fxf.project_form_instances.filter(instance__id__gt=instance).\
         values_list('instance', flat=True).order_by('pk')
